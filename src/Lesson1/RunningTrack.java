4780c57a8e6b3b651503db87b924b0922a513069
Cinnabon <anni_agust@mail.ru>
2021-05-26 17:34:32 +0300

    ДЗ№1

---
+++ src/Lesson1/RunningTrack.java
@@ -0,0 +1,26 @@
+package Lesson1;
+
+import HW1.Obstacles;
+
+public class RunningTrack implements Obstacles {
+    int length;
+
+    public RunningTrack(int length) {
+        this.length = length;
+    }
+
+    @Override
+    public boolean toRun(int maxLength) {
+        return (maxLength >= length);
+    }
+
+    @Override
+    public boolean toJump(int maxHeight) {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Дорожка длиной" + length + "м";
+    }
+}
